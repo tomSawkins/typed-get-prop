59c544c264f87dd0446bfada978815e9b932054e
Thomas Sawkins <sawkins.tom@gmail.com>
2019-03-18 20:46:33 +1100

    ci(package): install coveralls

--- support/package.ts
+++ support/package.ts
@@ -2,14 +2,13 @@ import * as fs from 'fs';
 import { rootPath } from 'get-root-path';
 import * as path from 'path';
 
-const filesToCopy = [
-  'package.json',
-  'README.md',
-  'LICENSE'
-]
+const filesToCopy = ['package.json', 'README.md', 'LICENSE'];
 
-filesToCopy.forEach(fileName => {
-  fs.copyFileSync(path.join(rootPath, fileName), path.join(rootPath, 'dist', fileName));
+filesToCopy.forEach((fileName) => {
+  fs.copyFileSync(
+    path.join(rootPath, fileName),
+    path.join(rootPath, 'dist', fileName)
+  );
 });
 
 const packageObject = JSON.parse(fs.readFileSync('dist/package.json', 'utf8'));
@@ -19,4 +18,8 @@ delete packageObject.scripts;
 delete packageObject.devDependencies;
 delete packageObject.husky;
 
-fs.writeFileSync(path.join(rootPath, 'dist/package.json'), JSON.stringify(packageObject, null, '  '), 'utf8');
+fs.writeFileSync(
+  path.join(rootPath, 'dist/package.json'),
+  JSON.stringify(packageObject, null, '  '),
+  'utf8'
+);
